eae34ed3c8a570bf921bd155d9dda096a003cc52
Nicolas Lohya <nicolas.lohya.pro@gmail.com>
2024-10-01 16:44:29 +0200

    :sparkles: Add parking service fake data

--- src/composables/parking.service.ts
+++ src/composables/parking.service.ts
@@ -5,7 +5,15 @@ export const useParkingService = () => {
 		get: async (): Promise<Parking[]> => {
 			return new Promise<Parking[]>((resolve) => {
 				resolve([
-					{ id: "123456789", name: "Parking" } as Parking,
+					{ id: "123456789", name: "Parking", busStations: [ {name: "Bus station", nextPassage: "10h"} ], metroStations: [ {name: "Metro station", nextPassage: "12h"} ], openingCalendar: {
+						monday: { opening: "10h", closure: "11h"},
+						tuesday: { opening: "10h", closure: "11h"},
+						wednesday: { opening: "10h", closure: "11h"},
+						thursday: { opening: "10h", closure: "11h"},
+						friday: { opening: "10h", closure: "11h"},
+						saturday: { opening: "10h", closure: "11h"},
+						sunday: { opening: "10h", closure: "11h"},
+						}} as Parking,
 				])
 			});
 		},
