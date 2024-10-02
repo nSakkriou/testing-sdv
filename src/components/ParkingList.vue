2ee058d65e227ea3ba358aa4444f85825f4e6e5a
Nicolas Lohya <nicolas.lohya.pro@gmail.com>
2024-10-02 13:38:30 +0200

    :sparkles: Add parking service fake data

---
+++ src/components/ParkingList.vue
@@ -0,0 +1,21 @@
+<template>
+
+</template>
+
+<script setup lang="ts">
+
+import {ref, Ref} from "vue";
+import {Parking} from "../models/Parking.ts";
+import {useParkingService} from "../composables/parking.service.ts";
+
+const parkingList: Ref<Array<Parking>> = ref([]);
+
+useParkingService().get().then((data: Parking[]) => {
+  parkingList.value = data;
+});
+
+</script>
+
+<style scoped>
+
+</style>
