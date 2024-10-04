ee16e994b55e36f559be8c987ab4e283a08ba12b
Nathan SAKKRIOU <nathansakkriou@gmail.com>
2024-10-04 12:10:08 +0200

    tEST in pipeline

---
+++ test.sh
@@ -0,0 +1,10 @@
+python3 -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
+
+while ! curl -s http://localhost:8080; do
+  sleep 1
+done
+
+mkdir ./test/output/
+robot --variable park_id:PRE -d test/output test/_.robot
